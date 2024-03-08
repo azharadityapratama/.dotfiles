8d1165371729c60fe3cae80e988da6744a3096f3
Azhar <azharadityapratama48@gmail.com>
2024-03-08 19:43:00 +0700

    edit none-ls

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,33 +1,27 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.diagnostics.mypy,
-					null_ls.builtins.formatting.black,
-					null_ls.builtins.formatting.isort,
-					null_ls.builtins.formatting.stylua,
-				},
-			})
-		end,
-	},
-	{
-		"jay-babu/mason-null-ls.nvim",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"nvimtools/none-ls.nvim",
-		},
-		config = function()
-			require("mason-null-ls").setup({
-				ensure_installed = {
-          "mypy",
-					"black",
-					"isort",
-					"stylua",
-				},
-			})
-		end,
-	},
+  "jay-babu/mason-null-ls.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "nvimtools/none-ls.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.stylua,
+      },
+    })
+
+    require("mason-null-ls").setup({
+      ensure_installed = {
+        "mypy",
+        "black",
+        "isort",
+        "stylua",
+      },
+    })
+  end,
 }
